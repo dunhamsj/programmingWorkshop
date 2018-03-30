988acff07e4fca6d6dc09c2114855a642ae6e5c7
Sam Dunham <samueljdunham@gmail.com>
2018-03-30 14:00:05 -0500

    ~/Desktop/ProgrammingWorkshop folder backup on Fri Mar 30 14:00:05 CDT 2018

--- ShadedPlot.py
+++ ShadedPlot.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 1000
+N  = 1000
 x0 = -5.0
 xf = 5.0
 
@@ -14,14 +14,16 @@ err1 = 0.1 * np.ones( N )
 err2 = 0.2 * np.ones( N )
 err3 = 0.3 * np.ones( N )
 
+a = 0.1
+c = 'b'
 plt.plot( x , y , 'b-' , label = 'Data' )
 
-plt.fill_between( x , y - err1 , y + err1 , color = 'b' , alpha = 0.5 , label = r'$1\sigma$' )
-plt.fill_between( x , y - err2 , y + err2 , color = 'b' , alpha = 0.5 , label = r'$2\sigma$' )
-plt.fill_between( x , y - err3 , y + err3 , color = 'b' , alpha = 0.5 , label = r'$3\sigma$' )
+plt.fill_between( x , y - err1 , y + err1 , color = c , alpha = a )
+plt.fill_between( x , y - err2 , y + err2 , color = c , alpha = a )
+plt.fill_between( x , y - err3 , y + err3 , color = c , alpha = a )
 
 plt.xlabel( r'$x$' )
 plt.ylabel( r'$\sin\left(x\right)$' )
 plt.legend()
-plt.savefig( 'Errors.png' )
-#plt.show()
+#plt.savefig( 'Errors.png' )
+plt.show()
